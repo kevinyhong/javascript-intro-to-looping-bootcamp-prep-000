134864f9c733ddf0e31515b0f070debff28aea0b
kevinyhong <kevinyaohuiong@gmail.com>
2018-07-17 18:34:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 function forLoop(array) {
   for (i = 0, i < 25; i++) {
-    array = array.push('I am ${i} strange loop.')
+    if(i === 0) {
+      array = array.push('I am ${i} strange loop.')
+    } else {
+      array = array.push('I am ${i} strange loops.')
+    }
+    
   }
   return array
 }
